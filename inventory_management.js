6b526f039ff5c29953414cda6d2845a061c5f024
= <=>
2024-09-12 13:36:43 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,13 @@
+// Task 1: Create an Inventory Array of Product Objects
+
+const inventory = [
+{ name: 'Chocolate', price: 5, quantity: 120, lowStockLevel: 10 },
+{ name: 'Lolipop', price: 1, quantity: 56, lowStockLevel: 12 },
+{ name: 'Candy', price: 2, quantity: 88, lowStockLevel: 15 },
+{ name: 'Cookies', price: 6, quantity: 211, lowStockLevel: 21 },
+{ name: 'Gummie bears', price: 3, quantity: 27, lowStockLevel: 16 }
+];
+
+
+
+
