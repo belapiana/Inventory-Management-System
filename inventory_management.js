4b1de5b9070c7714f8b10b00358818982458a4c7
= <=>
2024-09-15 14:18:32 -0400

    Initialize inventory with product objects

--- inventory_management.js
+++ inventory_management.js
@@ -8,63 +8,63 @@ const inventory = [
 { name: 'Gummie bears', price: 3, quantity: 27, lowStockLevel: 16 }
 ];
 
-// Task 2: Create a Function to Display Product Details
+// // Task 2: Create a Function to Display Product Details
 
-function displayProductDetails (input) {
-    const product = inventory.find(element => element.name === input);
-    console.log(`Product: ${product.name}`)
-    console.log(`Price:$${product.price}`)
-    console.log(`Quantity:${product.quantity}`)
-    if (product.quantity > product.lowStockLevel) 
-        {console.log(`In Stock`)}
-    else {console.log(`Low Stock`)}
-};
+// function displayProductDetails (input) {
+//     const product = inventory.find(element => element.name === input);
+//     if (product) {
+//     console.log(`Product: ${product.name}`);
+//     console.log(`Price:$${product.price}`);
+//     console.log(`Quantity:${product.quantity}`);
+//     console.log(product.quantity > product.lowStockLevel ? `In Stock` :`Low Stock`)}
+//     else {console.log(`Product not found`)};
+// };
 
-displayProductDetails("Chocolate");
+// displayProductDetails("Chocolate");
 
-// Task 3: Create a Function to Update Product Stock After Sales
+// // Task 3: Create a Function to Update Product Stock After Sales
 
-function updateStock (input, unitsSold) {
-    product = inventory.find(element => element.name === input);
-    console.log(`New quantity after units sold: ${product.quantity-unitsSold}`)
-    if (product.quantity > product.lowStockLevel) 
-        {console.log(`In Stock`)}
-    else {console.log(`Low Stock`)}
-};
+// function updateStock (input, unitsSold) {
+//     product = inventory.find(element => element.name === input);
+//     console.log(`New quantity after units sold: ${product.quantity-unitsSold}`)
+//     if (product.quantity > product.lowStockLevel) 
+//         {console.log(`In Stock`)}
+//     else {console.log(`Low Stock`)}
+// };
 
-updateStock("Chocolate", 24);
+// updateStock("Chocolate", 24);
 
-// Task 4: Create a Function to Check Low Stock Products
+// // Task 4: Create a Function to Check Low Stock Products
 
-function checkLowStock (inventory) {
-let allInStock= true
-    inventory.forEach(item => {
-    if  (item.quantity < item.lowStockLevel) 
-      {console.log(`${item.name} is low on stock!`);
-        allInStock=false}
-  });
-  if (allInStock===true) {console.log("All items are in Stock")} else {}
-};
+// function checkLowStock (inventory) {
+// let allInStock= true
+//     inventory.forEach(item => {
+//     if  (item.quantity < item.lowStockLevel) 
+//       {console.log(`${item.name} is low on stock!`);
+//         allInStock=false}
+//   });
+//   if (allInStock===true) {console.log("All items are in Stock")} else {}
+// };
 
-checkLowStock(inventory)
+// checkLowStock(inventory)
 
-// Task 5: Create a Function to Calculate Total Inventory Value
+// // Task 5: Create a Function to Calculate Total Inventory Value
 
-function calculateInventoryValue (inventory) {
-let totalValue = 0
-inventory.forEach (item => {
-totalValue += (item.quantity * item.price)
-});
-console.log(`Total Inventory Value: ${totalValue}`)
-};
+// function calculateInventoryValue (inventory) {
+// let totalValue = 0
+// inventory.forEach (item => {
+// totalValue += (item.quantity * item.price)
+// });
+// console.log(`Total Inventory Value: ${totalValue}`)
+// };
 
-calculateInventoryValue(inventory)
+// calculateInventoryValue(inventory)
 
-// Task 6: Create a Function to Process a Sale
+// // Task 6: Create a Function to Process a Sale
 
-function processSale (input, unitsSold) {
-    const product = inventory.find(element => element.name === input);
-    updateStock(product, unitsSold);
-};
+// function processSale (input, unitsSold) {
+//     const product = inventory.find(element => element.name === input);
+//     updateStock(product, unitsSold);
+// };
 
-console.log(processSale ("Gummie Bears, 7"));
+// console.log(processSale ("Gummie Bears, 7"));
